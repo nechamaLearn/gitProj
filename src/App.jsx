fd22001ca7e1bea4be56ceec8b531c5cc234f4be
nechamaLL <nechama.l@gmail.com>
2023-10-25 16:00:39 +0300

    remove botton

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,7 @@ function App() {
         </a>
       </div>
       <h1>VITE + REACT</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      hi
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
